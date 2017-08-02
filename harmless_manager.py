558b2a67cd5a3fa602587fa4b037793d59294e03
muuurk <szalay91@gmail.com>
2017-08-02 13:24:00 +0200

    bugfix

--- harmless_manager.py
+++ harmless_manager.py
@@ -292,7 +292,8 @@ def offline_mode(config):
 def start_virtual_switches(patch_port_num,trunk_port_num):
     print "Stop previously started virtual switch if it's alive"
     subprocess.call("./stop_ovs.sh " + str(patch_port_num) + " " + str(trunk_port_num), shell=True,stderr=False)
-
+    print '------------------------------------------------------------------------------------------------------------'
+    
     print "Create and start virtual switches"
     subprocess.call("./virtual_switch_starter_U16.sh " + str(patch_port_num) + " " + str(trunk_port_num),shell=True,stderr=False)
     #subprocess.call("./star_virtual_switch.sh " + str(patch_port_num) + " " + str(trunk_port_num), shell=True)
