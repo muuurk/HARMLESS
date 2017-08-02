07cdca5d5ce67aaaedb2823eecea7495faa6ecf8
muuurk <szalay91@gmail.com>
2017-08-02 12:54:30 +0200

    Add OVS starting script on Ubuntu 16

--- virtual_switch_starter_U16.sh
+++ virtual_switch_starter_U16.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 DBR="SoftSW_1"
 DBR2="SoftSW_2"
-#modprobe openvswitch
 
 function print_help
 {
@@ -9,11 +8,6 @@ function print_help
   echo -e "Usage: ./virtual_switch_starter.sh <number_of_patch_ports> <number_of_trunk_ports>"
   exit -1
 }
-
-#if [ $# -lt 1 ]
-#then
-#  print_help "Not enough paramaters"
-#fi
 
 #Read from configuration file
 config_file="configuration_file.ini"
@@ -66,10 +60,10 @@ echo "Delete preconfigured ovs data"
 sudo rm -rf /usr/local/etc/openvswitch/conf.db
 
 echo "Create ovs database structure"
-sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db  /usr/local/share/openvswitch/vswitch.ovsschema
+sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db     /usr/share/openvswitch/vswitch.ovsschema
 
 echo "Start ovsdb-server..."
-sudo ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --pidfile --detach
+sudo ovsdb-server /usr/local/etc/openvswitch/conf.db --remote=punix:/usr/local/var/run/openvswitch/db.sock     --remote=db:Open_vSwitch,Open_vSwitch,manager_options     --private-key=db:Open_vSwitch,SSL,private_key     --certificate=db:Open_vSwitch,SSL,certificate     --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert     --pidfile --detach --log-file
 
 echo "Initializing"
 sudo ovs-vsctl --no-wait init
@@ -78,20 +72,17 @@ echo "exporting environmental variable"
 export DB_SOCK=/usr/local/var/run/openvswitch/db.sock
 
 echo "start vswitchd..."
-sudo ovs-vswitchd --dpdk -c $c -n 4 -m 4096 -- unix:$DB_SOCK --pidfile --detach
-#sudo ovs-vswitchd  unix:$DB_SOCK --pidfile --detach
+sudo ovs-vswitchd unix:$DB_SOCK --pidfile --detach --log-file
 
 echo "Create dpdk-bridges (dpdk_br)"
 sudo ovs-vsctl add-br $DBR -- set bridge $DBR datapath_type=netdev
 sudo ovs-vsctl add-br $DBR2 -- set bridge $DBR2 datapath_type=netdev
-#sudo ovs-vsctl add-br $DBR
-#sudo ovs-vsctl add-br $DBR2
 
 echo "Delete flows from dpdk_br"
 sudo ovs-ofctl del-flows $DBR
 sudo ovs-ofctl del-flows $DBR2
 
-echo "Add dpdk ports ports"
+echo "Add logical patch ports"
 
 for pp in $(seq 1 $patch_ports_count)
 do
@@ -105,7 +96,7 @@ do
   cmd4="sudo ovs-vsctl set Interface patch-ovs-${vlan} options:peer=patch-ovs-${pp}"
   $cmd4
 done
-
+echo "Add physical trunk ports"
 for i in $(seq 1 $trunk_ports_count)
 do
     trunk=`echo $trunk_ports | cut -f1 -d','`
@@ -135,7 +126,6 @@ fi
 
 echo "Adding forwarding rules to ${DBR}"
 
-#patch_links=`expr 2 \* $patch_ports_count`
 phy_if_out=`expr $patch_ports_count + 1`
 part_math=`expr $trunk_ports_count - 1`
 part_math2=`expr $patch_ports_count + $part_math`
@@ -144,12 +134,12 @@ for_one_trunk=`expr $part_math2 / $trunk_ports_count`
 for pp in $(seq 1 $patch_ports_count)
 do
   vlan=$(expr 100 + $pp)
-  #output=$(expr $pp + 1)
   add_port="sudo ovs-ofctl add-flow ${DBR} dl_vlan=${vlan},actions=strip_vlan,output:${pp}"
   $add_port
-  #backDirection=$(expr $output + $patch_ports_count)
+
   next_if=`expr $patch_ports_count % $for_one_trunk`
   add_port2="sudo ovs-ofctl add-flow ${DBR} in_port=${pp},actions=mod_vlan_vid:${vlan},output:${phy_if_out}"
+
   if [ "$next_if" == 0 ]
   then
     phy_if_out=`expr $phy_if_out + 1`
